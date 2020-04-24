319e1f6e8336e32a9b4b2722e0fd090986a55a62
Tony Collins <tjc@eltham-college.org.uk>
2020-04-24 12:41:52 +0100

    Add project files.

---
+++ WindowsFormsGUIDecoupled/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsGUIDecoupled
+{
+    class Game
+    {
+        private Boolean clickedButton = false;
+        public Game someoneHasClickedMyButton()
+        {
+
+            // this event could call metthods in other classes but as a demo it's
+            // just going to change the button clicked value to true
+            // It could return a reference to the entire game to the GUI so get set methods could be used
+            // by the gui to access any data in the game to update the gui
+            // in fact i'll return a reference to the object rather than the boolean 
+            clickedButton = true;
+            return this;
+        }
+
+        public Boolean getClickedButton()
+        {
+           return  clickedButton ;
+        }
+    }
+}
